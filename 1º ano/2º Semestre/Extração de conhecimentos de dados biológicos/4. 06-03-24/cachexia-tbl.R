3844cafa6c5a8c53779692e6bef9f0edd87d9740
B-Neil <joaoferreira.ac23@gmail.com>
2024-03-06 16:10:21 +0000

    up

--- 1º ano/2º Semestre/Extração de conhecimentos de dados biológicos/4. 06-03-24/cachexia-tbl.R
+++ 1º ano/2º Semestre/Extração de conhecimentos de dados biológicos/4. 06-03-24/cachexia-tbl.R
@@ -7,11 +7,14 @@ download.file("http://darwin.di.uminho.pt/dataAnalysisR/meta_cachexia.csv","meta
 
 cachexia = read.table("cachexia.csv", sep = ",", header = T, row.names = 1)
 meta_cachexia = read.table("meta.csv", sep = ",", header = T, row.names = 1)
+cachexia.log= log10(cachexia)
 cachexia = cbind(cachexia, meta_cachexia)
 dim(cachexia)
 
 
 # check normal distribution
+#a avaliar a normalidade das várias colunas
+#b há 63 colunas que não seguem a normalidade
 
 pv.orig = sapply(cachexia[,1:63], function(x) shapiro.test(x)$p.value)
 sum(pv.orig < 0.01)
@@ -19,7 +22,15 @@ pv.log = sapply(cachexia.log[,1:63], function(x) shapiro.test(x)$p.value)
 sum(pv.log < 0.01)
 colnames(cachexia.log)[which(pv.log < 0.01)]
 
+
+
+
+#2.1 analisar a expressão diferencial - #2. - Estamos a agrupar os dataset- os dados e os meta_dados e fazendo um teste de media comparando os valores de controlo com os "doentes" -  vamos separar as variaeis pelos dois grupos e fazer o t.test com o objetivo para avalair se há diferenças significativas nas medias do grupo 1 e do grupo 2.
+#2.2- b - deveriamos verificar a normalidade pq dependendo deste parametro deveriamos usar um tipo diferente de teste usando o teste nao parametrico para variaveis que nao seguem a normalidade. A desvantagem e usar testes nao parametricos são menos potentes rejeitando menos vezes a hipotese nula. - No caso de seguir uma distribuição normal faziamos um t.test mas no caso que não faziamos um wilcox test. para verificar a normalidade fazeriamos um teste de shapiro.
+
+
 # differential expression analysis
+ds = merge(cachexia,meta_cachexia)
 
 testCompound = function(ds, index) ## parametric
 {
@@ -70,3 +81,6 @@ pvs_adj = p.adjust(pvs, "BH") # "BH" more conservative
 sum(pvs < 0.01)
 sum(pvs_adj < 0.01)
 hist(-log10(pvs_adj), breaks = 0:6, col = "orange")
+
+
+#3- 
